a8c7d30e9dbfb6120ca7e12620e843320e6349eb
Pedro Silva <73141072+Pedro-22001798@users.noreply.github.com>
2023-09-25 16:27:43 +0100

    Remove Levels comments

--- PipeChallenge/Assets/Scripts/Level/Levels.cs
+++ PipeChallenge/Assets/Scripts/Level/Levels.cs
@@ -18,38 +18,18 @@ public class Levels : MonoBehaviour
                       "empty,0,curve,90,curve,180\n" +
                       "empty,0,empty,0,end,180");
 
-        // Maps.Add("light,-90,empty,0,empty,0\n" +
-        //               "curve,0,normal,90,curve,180\n" +
-        //               "empty,0,empty,0,end,180");
+        Maps.Add("light,-90,empty,0,empty,0\n" +
+                      "curve,0,normal,90,curve,180\n" +
+                      "empty,0,empty,0,end,180");
 
-        // Maps.Add("light,0,normal,90,end,180\n" +
-        //               "light,0,normal,90,end,180\n" +
-        //               "light,0,normal,90,end,180");
+        Maps.Add("light,0,normal,90,end,180\n" +
+                      "light,0,normal,90,end,180\n" +
+                      "light,0,normal,90,end,180");
 
-        // Maps.Add("empty,0,empty,0,empty,0,empty,0,end,90\n" +
-        //          "light,0,normal,90,normal,90,curve,90,normal,180\n" +
-        //          "empty,0,empty,0,empty,0,curve,90,curve,180\n" +
-        //          "empty,0,curve,90,curve,180,empty,0,light,270\n" +
-        //          "end,0,curve,90,curve,180,normal,0,curve,90");
-
-        // Maps.Add("light,0,normal,90,end,180\n" +
-        //               "light,0,normal,90,end,180\n" +
-        //               "light,0,normal,90,end,180");
-
-        // Maps.Add("light,0,normal,90,end,180\n" +
-        //               "light,0,normal,90,end,180\n" +
-        //               "light,0,normal,90,end,180");
-
-        // Maps.Add("light,0,normal,90,end,180\n" +
-        //               "light,0,normal,90,end,180\n" +
-        //               "light,0,normal,90,end,180");
-
-        // Maps.Add("light,0,normal,90,end,180\n" +
-        //               "light,0,normal,90,end,180\n" +
-        //               "light,0,normal,90,end,180");
-
-        // Maps.Add("light,0,normal,90,end,180\n" +
-        //               "light,0,normal,90,end,180\n" +
-        //               "light,0,normal,90,end,180");
+        Maps.Add("empty,0,empty,0,empty,0,empty,0,end,90\n" +
+                 "light,0,normal,90,normal,90,curve,90,normal,180\n" +
+                 "empty,0,empty,0,empty,0,curve,90,curve,180\n" +
+                 "empty,0,curve,90,curve,180,empty,0,light,270\n" +
+                 "end,0,curve,90,curve,180,normal,0,curve,90");
     }
 }

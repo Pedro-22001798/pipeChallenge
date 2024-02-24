35c8ce1edc50b7742118179ee00e94edc5de9aa6
Pedro Silva <73141072+Pedro-22001798@users.noreply.github.com>
2024-02-24 20:02:42 +0000

    Game Controller

--- PipeChallenge/Assets/Scripts/GameController.cs
+++ PipeChallenge/Assets/Scripts/GameController.cs
@@ -4,5 +4,49 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    
+    public static GameController Instance {get; private set;}
+    [SerializeField] private GameObject mainMenu, game, options;
+    [SerializeField] private Animator mainMenuAnimator, gameAnimator, optionsAnimator;
+
+    void Awake()
+    {
+        if(Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    public void OpenMainMenu()
+    {
+
+    }
+
+    public void CloseMainMenu()
+    {
+
+    }
+
+    public void OpenGame()
+    {
+
+    }
+
+    public void CloseGame()
+    {
+
+    }
+
+    public void OpenOptions()
+    {
+
+    }
+
+    public void CloseOptions()
+    {
+
+    }
 }
